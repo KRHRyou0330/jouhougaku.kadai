8f0535524a0304c756dc4251035cb9fbff1b49cb
KRHRyou0330 <zm19303@josai.ac.jp>
2021-02-02 06:10:57 +0000

    2/2

---
+++ josai.js
@@ -0,0 +1,12 @@
+function sayhello1(){
+    alert('Hello, ' +
+    document.getElementById('who').value + '!');
+}
+
+function imgchange1() {
+    document.getElementById('change').src= './image/imohurai.jpg';
+}
+
+function imgchange2() {
+    document.getElementById('change').src= './image/sanora-men.jpg';
+}
